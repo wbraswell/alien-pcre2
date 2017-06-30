5860e9a58d7789311bf7b13c55de76cc77c52461
Will Braswell <william.braswell@autoparallel.com>
2017-06-30 05:14:49 -0500

    Initial Commit, All Critical Files

---
+++ lib/Alien/PCRE2.pm
@@ -0,0 +1,48 @@
+use strict;
+use warnings;
+package Alien::PCRE2;
+
+our $VERSION = '0.001000';
+
+use base qw( Alien::Base );
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Alien::PCRE2 - Find or download/build/install libpcre2 in PCRE2, the new Perl Compatible Regular Expression engine
+
+=head1 SYNOPSIS
+
+From a Perl script
+
+ use Alien::PCRE2;
+ use Env qw(@PATH);
+ unshift @PATH, Alien::PCRE2->bin_dir();  # `pcre2-config` and `pcre2grep` commands are now in your path
+
+From Alien::Base Makefile.PL
+
+    ADD CODE HERE
+
+=head1 DESCRIPTION
+
+This package can be used by other CPAN modules that require PCRE2 or libpcre2.
+
+=head1 AUTHOR
+
+William N. Braswell, Jr. <wbraswell@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2017 by William N. Braswell, Jr.;
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
